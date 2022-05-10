448bb5b3dda82cc40661469935deafec159f5c22
Michael Antoniades <scmikeyt@yahoo.com>
2022-05-10 17:12:07 -0400

    Profile picture drop down functionality

--- src/MainSec.js
+++ src/MainSec.js
@@ -1,23 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import './MainSec.css';  
 import axios from "axios"
-import { ImNext } from 'react-icons/im';
-import {GiCircle} from 'react-icons/gi' 
-import {FaUserAlt} from 'react-icons/fa'
 
+import {FaUserCircle} from 'react-icons/fa'
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 function MainSec () {
     const [posts, setPosts] = useState([]);
 
-    // useEffect(()=> {
-    //     const fetchPosts = async()=> {
-    //         const res = await axios.get("/login")
-    //         console.log(res)
-    //         setPosts(res.data);
-    //     }
-    //     fetchPosts();
-    // },[])
+// Dropdown Menu
+
+    useEffect(()=> {
+        const profileDropDown = async()=> {
+            const $dropdownArrow = document.querySelector('.fa-angle-down');
+            const $checkbox = document.getElementById('openDropdown');
+            console.log($checkbox)
+            const $dropdownMenu = document.querySelector('.dropdown-menu');
+            
+            $checkbox.addEventListener('change', () => {
+              $dropdownArrow.classList.toggle('rotate-dropdown-arrow');
+            });
+            
+            $dropdownMenu.addEventListener('click', (e) => {
+              $checkbox.checked = false;
+              // setting checked to false won't trigger 'change'
+              // event, manually dispatch an event to rotate
+              // dropdown arrow icon
+              $checkbox.dispatchEvent(new Event('change'));
+            });
+        }
+        profileDropDown();
+    },[])
 
     // const getLoginAuth = async () => {
     //     try { 
@@ -33,24 +48,46 @@ function MainSec () {
         
     // }
 
+    // const $dropdownArrow = document.querySelector('.fa-angle-down');
+    // const $checkbox = document.getElementById('openDropdown');
+    // console.log($checkbox)
+    // const $dropdownMenu = document.querySelector('.dropdown-menu');
+    
+    // $checkbox.addEventListener('change', () => {
+    //   $dropdownArrow.classList.toggle('rotate-dropdown-arrow');
+    // });
+    
+    // $dropdownMenu.addEventListener('click', (e) => {
+    //   $checkbox.checked = false;
+    //   // setting checked to false won't trigger 'change'
+    //   // event, manually dispatch an event to rotate
+    //   // dropdown arrow icon
+    //   $checkbox.dispatchEvent(new Event('change'));
+    // });
 
     //href = 'http://localhost:8080'
     return (
         <>
             <div className = 'backsplash'>
-                <a className = "login" href = 'http://localhost:8080' > 
-                    <GiCircle color = {'#8A8A8A'} stroke-width = {5} size = {45} textAnchor="middle" alignmentBaseline="middle"  />
-                    <FaUserAlt size = {25} color = {'#8A8A8A'}
-                        textAnchor="middle"
-                        alignmentBaseline="middle"
-                        style={{ position: 'absolute', left: '.7em', bottom: '.75em', zIndex: '-1' }}   
-                    />
-                </a>  
-                    {/* <img 
-                    className = 'purplebeach'
-                    src = 'images/purplebeach.jpg' 
-                    alt='purplebeach'>
-                    </img> */}
+            {/* href = 'http://localhost:8080' */}
+                <div className = "login"  > 
+                    <div class="dropdown-container">
+                        <label for="openDropdown" className="dropdown">
+                        <FaUserCircle class = "userCircle" size = {30}  />
+                            <i className="fa fa-angle-down"></i>
+                        </label>
+                        
+                        <input type="checkbox" id="openDropdown" hidden>
+                        </input>
+                        <div class="dropdown-menu">
+                            <span>Item 1</span>
+                            <span>Item 1</span>
+                            <span>Item 1</span>
+
+                        </div>
+                    </div>
+
+                </div>  
             </div>
         </>
     );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
+import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
